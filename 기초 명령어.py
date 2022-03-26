6069138585c97e436e3215c786d40b2c105e3024
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-03-26 12:13:56 +0900

    Add files via upload

---
+++ 기초 명령어.py
@@ -0,0 +1,23 @@
+{
+"terminal.integrated.profiles.windows": {
+    "GitBash": {
+      "path": "C:\\Program Files\\Git\\bin\\bash.exe",
+      "icon": "terminal-bash"
+    },
+    "PowerShell": {
+      "source": "PowerShell",
+      "icon": "terminal-powershell"
+    },
+    "Command Prompt": {
+      "path": "C:\\WINDOWS\\System32\\cmd.exe",
+      "icon": "terminal-cmd"
+    }
+  },
+  "terminal.integrated.defaultProfile.windows": "PowerShell",
+  "workbench.colorTheme": "Beard Theme Monokai Stone",
+  "workbench.iconTheme": "material-icon-theme"
+} # VS Code settings.json file : Git Bash의 기본 터미널 적용 명령어
+
+
+
+
