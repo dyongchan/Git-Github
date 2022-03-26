1b4deb63b875d9b7c33369ccbbb9ed6796ec1368
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-03-26 12:15:21 +0900

    수정

--- 기초 명령어.py
+++ 기초 명령어.py
@@ -16,7 +16,9 @@
   "terminal.integrated.defaultProfile.windows": "PowerShell",
   "workbench.colorTheme": "Beard Theme Monokai Stone",
   "workbench.iconTheme": "material-icon-theme"
-} # VS Code settings.json file : Git Bash의 기본 터미널 적용 명령어
+}
+# VS Code settings.json file : Git Bash의 기본 터미널 적용 명령어
+# Git Bash를 이용하여 Linux체제의 명령어도 실행가능
 
 
 
