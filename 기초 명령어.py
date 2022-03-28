d7e9491a033a9861b19b2f248b6be0aca2294a48
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-03-29 01:53:10 +0900

    Add files via upload

--- 기초 명령어.py
+++ 기초 명령어.py
@@ -1,3 +1,6 @@
+import email
+
+
 {
 "terminal.integrated.profiles.windows": {
     "GitBash": {
@@ -20,7 +23,18 @@
 # VS Code settings.json file : Git Bash의 기본 터미널 적용 명령어
 # Git Bash를 이용하여 Linux체제의 명령어도 실행가능
 
-# git bash 명령어
-1. git init : 내가 만든 local workspace에 .git 파일 생성 및 초기화
+1. 설치 및 버전확인, 기초 폴더 생성
+git --vsesion : git의 설치여부를 알 수 있으며 버전확인 가능
+git init : 로컬 작업폴더 내의 git 관리 및 정보폴더 (.git) 생성
 
+2. 협업 시 윈도우와 맥의 엔터 방식 차이에 의한 오류 방지
+git config --global core.autocrlf input
 
+3. 사용자 이름 및 이메일 주소 설정 브랜치명 변경
+git config --global user.name "(본인 이름)"
+git config --global user.email "(본인 이메일)"
+# 이름 및 주소 확인
+git config --global (user.name)
+git config --global (user.email)
+# 기본 브랜치명 변경
+git config --global init.defaultBranch (bracnch name)
