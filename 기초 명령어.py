ca7a5724caf719ca5c78dba47d6bf3fb919d12cc
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-04-02 00:55:47 +0900

    Add files via upload

--- 기초 명령어.py
+++ 기초 명령어.py
@@ -26,6 +26,7 @@ import email
 1. 설치 및 버전확인, 기초 폴더 생성
 git --vsesion : git의 설치여부를 알 수 있으며 버전확인 가능
 git init : 로컬 작업폴더 내의 git 관리 및 정보폴더 (.git) 생성
+git status : 현재상태를 git의 입장에서 나타냄
 
 2. 협업 시 윈도우와 맥의 엔터 방식 차이에 의한 오류 방지
 git config --global core.autocrlf input
@@ -38,3 +39,36 @@ git config --global (user.name)
 git config --global (user.email)
 # 기본 브랜치명 변경
 git config --global init.defaultBranch (bracnch name)
+
+4. git에게 맡기지 않을 파일 관리
+.gitignore 폴더 생성 후 내용삽입
+
+.gitignore의 대표적 사용 양식
+# 포함할 필요가 없는 파일 / 보안상 민감한 정보일 때
+
+# 이렇게 #를 사용해서 주석
+
+# 모든 file.c
+file.c
+
+# 최상위 폴더의 file.c
+/file.c
+
+# 모든 .c 확장자 파일
+*.c
+
+# .c 확장자지만 무시하지 않을 파일
+!not_ignore_this.c
+
+# logs란 이름의 파일 또는 폴더와 그 내용들
+logs
+
+# logs란 이름의 폴더와 그 내용들
+logs/
+
+# logs 폴더 바로 안의 debug.log와 .c 파일들
+logs/debug.log
+logs/*.c
+
+# logs 폴더 바로 안, 또는 그 안의 다른 폴더(들) 안의 debug.log
+logs/**/debug.log
